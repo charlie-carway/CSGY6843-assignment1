63c45d7d868185714fb7b6394d61956b40a8adf9
Charles Carway <78627703+charlie-carway@users.noreply.github.com>
2022-02-05 22:22:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -23,9 +23,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
          answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-         answer = "5"
-    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
          answer = "6"
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+         answer = "7"
 
     return(answer)
 # Complete all the questions.
@@ -35,17 +35,17 @@ if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
+#    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+#    print(welcome_assignment_answers(debug_question))
